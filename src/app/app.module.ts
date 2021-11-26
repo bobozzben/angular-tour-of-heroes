b106abb7c5838baefcb47fa2f12b366ef293089f
benhserp <ben@hserp.tw>
2021-11-26 14:04:06 +0800

    update to angular 13

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';  //  NgModel 雙向資料繫結要匯入
-
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 
 @NgModule({
@@ -19,9 +23,12 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     DashboardComponent
   ],
   imports: [
+    InMemoryDataService,
     BrowserModule,
     FormsModule,  //  NgModel 雙向資料繫結要匯入
-    AppRoutingModule  // app-routing.module.ts
+    AppRoutingModule,  // app-routing.module.ts
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService,{dataEncapsulation:false})
   ],
   providers: [],
   bootstrap: [AppComponent]
